a518a647565e6c1481f74261c6eace31d7f82376
unknown <jvaccaro@1H8YLX1.olin.edu>
2015-11-05 12:33:24 -0500

    Fixed FSM

--- Lab/Lab2/finitestatemachine.t.v
+++ Lab/Lab2/finitestatemachine.t.v
@@ -18,23 +18,37 @@ reg 	shiftRegOut0;
 			 .chipSel(chipSel),
 			 .shiftRegOut0(shiftRegOut0));
 
-    
+reg[7:0]	yolo;
     initial begin
-	genvar	yolo;
+
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
     // Synchronize, Clean, Preprocess (edge finding)
     // TEST CASE 1: Run the whole shebang 
-	chipSel = 1;
-	shiftRegOut0 = 0;
-	generate
-	for (yolo=0; yolo<30; yolo=yolo+1) begin
+	chipSel = 0;
+	shiftRegOut0 = 1;
+	$display("TEST 1: MISO || dataMem || addrWE || ShiftRegWE");
+	for (yolo=0; yolo<40; yolo=yolo+1) begin
 		clkEdge = 1; 
 		#30 
 		clkEdge=0;
 		$display("%b %b %b %b", MISO_buf, dataMem_WE, addr_WE, shiftReg_WE);
+		#500;
 	end
-	endgenerate
+	chipSel = 1;
+	clkEdge = 1;
+	#30
+	clkEdge = 0;
+	chipSel = 0;
+	shiftRegOut0 = 0;
+	$display("TEST 2: MISO || dataMem || addrWE || ShiftRegWE");
+	for (yolo=0; yolo<40; yolo=yolo+1) begin
+		clkEdge = 1; 
+		#30 
+		clkEdge=0;
+		$display("%b %b %b %b", MISO_buf, dataMem_WE, addr_WE, shiftReg_WE);
+		#500;
+	end
     end
 
 endmodule
--- Lab/Lab2/finitestatemachine.v
+++ Lab/Lab2/finitestatemachine.v
@@ -19,17 +19,15 @@ input 	shiftRegOut0
 
 reg[2:0] curState = 3'b000;
 reg[2:0] curCount = 3'b000;
-reg reset_Counter = 0;
 
 always @(posedge clkEdge) begin
 	MISO_buf <= 0;
 	dataMem_WE <= 0;
 	addr_WE <= 0;
 	shiftReg_WE <= 0;
-	reset_Counter <= 0;
 	if (chipSel) begin
 		curState <= 3'b000;
-		reset_Counter <= 1;
+		curCount <= 0;
 	end
 	case(curState)
 		3'b000 : 
@@ -42,12 +40,12 @@ always @(posedge clkEdge) begin
 		3'b001 : 
 			if (shiftRegOut0) begin
 				curState <= curState + 1;
-				reset_Counter <= 1;
+				curCount <= 0;
 				addr_WE <=1;
 			end
 			else begin
 				curState <= curState + 4;
-				reset_Counter <= 1;
+				curCount <= 0;
 				addr_WE <=1;
 			end
 		3'b010 : 
@@ -80,12 +78,8 @@ always @(posedge clkEdge) begin
 				dataMem_WE <= 1;
 			end
 		3'b111 : 
-			reset_Counter <= 1;
+			curCount <= 0;
 	endcase
-	if (reset_Counter) begin
-		curCount <= 0;
-		reset_Counter <=0;
-	end
 end
 
 endmodule
