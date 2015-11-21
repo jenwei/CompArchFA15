9e0c64bfc37fad206e831fe09d5a6dda16d2e289
Jacob Riedel <jr112394@gmail.com>
2015-11-20 21:46:12 -0500

    EDIT: changed case to if else if statements

--- Lab/Lab3/lut.v
+++ Lab/Lab3/lut.v
@@ -15,117 +15,116 @@ output reg	brch		// 1 enables branch, 0 does not
 always @(opcode or func)begin
 	case (opcode)
 		6'd0:begin //R-Type, use Func to determine command
-			case(func)
-				6'd32:begin //Add
-					RegDst = 0;
-					RegWr = 1;
-					ALUsrc = 1;
-					ALUcntrl = 3'b000; // ADD code
-					MemtoReg = 1;
-					MemWr = 0;
-					jmp = 0;
-					brch =0;
-				end
-				6'd34:begin //Subtract
-					RegDst = 0;
-					RegWr = 1;
-					ALUsrc = 1;
-					ALUcntrl = 3'b001; // Subtract Code
-					MemtoReg = 1;
-					MemWr = 0;
-					jmp = 0;
-					brch =0;
-				end
-				6'd42:begin //Set Less Than
-					RegDst = 0;
-					RegWr = 1;
-					ALUsrc = 1;
-					ALUcntrl = 3'b011; // SLT code
-					MemtoReg = 1;
-					MemWr = 0;
-					jmp = 0;
-					brch =0;
-				end
-				default:begin // Handles exceptions
-					RegDst = 0;
-					RegWr = 0; // Do not allow write to Reg
-					ALUsrc = 0;
-					ALUcntrl = 3'b000;
-					MemtoReg = 0;
-					MemWr = 0; // Do not allow write to dataMem
-					jmp = 0; // Do not jump
-					brch =0; // Do not branch
-				end
+			if (func == 6'd32) begin //Add
+				RegDst <= 0;
+				RegWr <= 1;
+				ALUsrc <= 1;
+				ALUcntrl <= 3'b000; // ADD code
+				MemtoReg <= 1;
+				MemWr <= 0;
+				jmp <= 0;
+				brch <= 0;
+			end
+			else if (func == 6'd34) begin //Subtract
+				RegDst <= 0;
+				RegWr <= 1;
+				ALUsrc <= 1;
+				ALUcntrl <= 3'b001; // Subtract Code
+				MemtoReg <= 1;
+				MemWr <= 0;
+				jmp <= 0;
+				brch <= 0;
+			end
+			else if (func == 6'd42)begin //Set Less Than
+				RegDst <= 0;
+				RegWr <= 1;
+				ALUsrc <= 1;
+				ALUcntrl <= 3'b011; // SLT code
+				MemtoReg <= 1;
+				MemWr <= 0;
+				jmp <= 0;
+				brch <= 0;
+			end
+			else begin // Handles exceptions
+				RegDst <= 0;
+				RegWr <= 0; // Do not allow write to Reg
+				ALUsrc <= 0;
+				ALUcntrl <= 3'b000;
+				MemtoReg <= 0;
+				MemWr <= 0; // Do not allow write to dataMem
+				jmp <= 0; // Do not jump
+				brch <= 0; // Do not branch
+			end
 		end
 		6'd2:begin // Jump
-			RegDst = 0;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b000;
-			MemtoReg = 0;
-			MemWr = 0;
-			jmp = 1; // Allow jump
-			brch =0;
+			RegDst <= 0;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b000;
+			MemtoReg <= 0;
+			MemWr <= 0;
+			jmp <= 1; // Allow jump
+			brch <= 0;
 		end
 		6'd3:begin //Jump and Link
-			RegDst = 0;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b000;
-			MemtoReg = 0;
-			MemWr = 0;
-			jmp = 1; // Allow jump
-			brch =0;
+			RegDst <= 0;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b000;
+			MemtoReg <= 0;
+			MemWr <= 0;
+			jmp <= 1; // Allow jump
+			brch <= 0;
 		end
 		6'd5:begin // Branch on Not Equal
-			RegDst = 0;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b001; // Subtract to test inequality
-			MemtoReg = 1;
-			MemWr = 0;
-			jmp = 0;
-			brch = 1; // Allow branch
+			RegDst <= 0;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b001; // Subtract to test inequality
+			MemtoReg <= 1;
+			MemWr <= 0;
+			jmp <= 0;
+			brch <= 1; // Allow branch
 		end
 		6'd6:begin // Jump Register
-			RegDst = 0;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b000;
-			MemtoReg = 0;
-			MemWr = 0;
-			jmp = 1; // Aloow jump
-			brch =0;
+			RegDst <= 0;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b000;
+			MemtoReg <= 0;
+			MemWr <= 0;
+			jmp <= 1; // Aloow jump
+			brch <= 0;
 		end
 		6'd14:begin // Exclusive Or Immediate
-			RegDst = 1;
-			RegWr = 1;
-			ALUsrc = 0;
-			ALUcntrl = 3'b010; // XOR code
-			MemtoReg = 1;
-			MemWr = 0;
-			jmp = 0;
-			brch =0;
+			RegDst <= 1;
+			RegWr <= 1;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b010; // XOR code
+			MemtoReg <= 1;
+			MemWr <= 0;
+			jmp <= 0;
+			brch <= 0;
 		end
 		6'd35:begin // Load
-			RegDst = 1;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b000;
-			MemtoReg = 1;
-			MemWr = 0;
-			jmp = 0;
-			brch =0;
+			RegDst <= 1;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b000;
+			MemtoReg <= 1;
+			MemWr <= 0;
+			jmp <= 0;
+			brch <= 0;
 		end
 		6'd43:begin // Store
-			RegDst = 0;
-			RegWr = 0;
-			ALUsrc = 0;
-			ALUcntrl = 3'b000;
-			MemtoReg = 1;
-			MemWr = 1;
-			jmp = 0;
-			brch =0;
+			RegDst <= 0;
+			RegWr <= 0;
+			ALUsrc <= 0;
+			ALUcntrl <= 3'b000;
+			MemtoReg <= 1;
+			MemWr <= 1;
+			jmp <= 0;
+			brch <= 0;
 		end
 		default:begin // Handles exceptions
 			RegDst = 0;
